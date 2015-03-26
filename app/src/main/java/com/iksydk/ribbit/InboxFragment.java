e451883869679c7f3f2571222ba5e469b6cf2a06
Billy Laney <billy3@gmail.com>
2015-03-26 00:07:12 -0500

    Add self destruct

--- app/src/main/java/com/iksydk/ribbit/InboxFragment.java
+++ app/src/main/java/com/iksydk/ribbit/InboxFragment.java
@@ -18,6 +18,7 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -114,6 +115,22 @@ public class InboxFragment extends ListFragment
         }
 
         //delete the image
+        List<String> ids = message.getList(ParseConstants.KEY_RECIPIENT_IDS);
+        if(ids.size() == 1)
+        {
+            //delete the message
+            message.deleteInBackground();
+        }
+        else
+        {
+            //remove recipient
+            ids.remove(ParseUser.getCurrentUser().getObjectId());
+            ArrayList<String> idsToRemove = new ArrayList<>();
+            idsToRemove.add(ParseUser.getCurrentUser().getObjectId());
+
+            message.removeAll(ParseConstants.KEY_RECIPIENT_IDS, idsToRemove);
+            message.saveInBackground();
+        }
     }
 
     public void showProgress(boolean show)
