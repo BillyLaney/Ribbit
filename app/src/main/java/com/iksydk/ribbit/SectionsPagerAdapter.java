d171f483b7975b3cf9a6ed90e8a7ca844c0d4f17
Billy Laney <billy3@gmail.com>
2015-03-26 23:07:25 -0500

    Updated the tab theme and added icons

--- app/src/main/java/com/iksydk/ribbit/SectionsPagerAdapter.java
+++ app/src/main/java/com/iksydk/ribbit/SectionsPagerAdapter.java
@@ -11,17 +11,20 @@ import java.util.Locale;
  * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
  * one of the sections/tabs/pages.
  */
-public class SectionsPagerAdapter extends FragmentPagerAdapter {
+public class SectionsPagerAdapter extends FragmentPagerAdapter
+{
 
     protected Context mContext;
 
-    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+    public SectionsPagerAdapter(Context context, FragmentManager fm)
+    {
         super(fm);
         mContext = context;
     }
 
     @Override
-    public Fragment getItem(int position) {
+    public Fragment getItem(int position)
+    {
         // getItem is called to instantiate the fragment for the given page.
         // Return a DummySectionFragment (defined as a static inner class
         // below) with the page number as its lone argument.
@@ -37,19 +40,36 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     }
 
     @Override
-    public int getCount() {
+    public int getCount()
+    {
         return 2;
     }
 
     @Override
-    public CharSequence getPageTitle(int position) {
+    public CharSequence getPageTitle(int position)
+    {
         Locale l = Locale.getDefault();
-        switch (position) {
+        switch(position)
+        {
             case 0:
-                return mContext.getString(R.string.title_section1).toUpperCase(l);
+                return mContext.getString(R.string.title_section1)
+                        .toUpperCase(l);
             case 1:
-                return mContext.getString(R.string.title_section2).toUpperCase(l);
+                return mContext.getString(R.string.title_section2)
+                        .toUpperCase(l);
         }
         return null;
+    }
+
+    public int getIcon(int position)
+    {
+        switch(position)
+        {
+            case 0:
+                return R.mipmap.ic_tab_inbox;
+            case 1:
+                return R.mipmap.ic_tab_friends;
+        }
+        return R.mipmap.ic_tab_inbox;
     }
 }
