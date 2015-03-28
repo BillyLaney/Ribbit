128381018a74e160697b66a21ed8c28aa2c328f4
Billy Laney <billy3@gmail.com>
2015-03-28 09:29:21 -0500

    optimize the imports

--- app/src/main/java/com/iksydk/ribbit/ui/InboxFragment.java
+++ app/src/main/java/com/iksydk/ribbit/ui/InboxFragment.java
@@ -9,8 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.iksydk.ribbit.R;
 import com.iksydk.ribbit.adapters.MessageAdapter;
