9587c19f9782c12ca24c698665a4d0ed19ee5ddd
Billy Laney <billy3@gmail.com>
2015-03-29 19:10:23 -0500

    Pushing messaging fix. Parse got bugs

--- app/src/main/java/com/iksydk/ribbit/RibbitApplication.java
+++ app/src/main/java/com/iksydk/ribbit/RibbitApplication.java
@@ -1,19 +1,26 @@
 package com.iksydk.ribbit;
 
 import android.app.Application;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.iksydk.ribbit.ui.MainActivity;
 import com.iksydk.ribbit.utils.ParseConstants;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
 import com.parse.ParseUser;
 import com.parse.PushService;
+import com.parse.SaveCallback;
 
 /**
  * Created by Billy on 3/24/2015.
  */
 public class RibbitApplication extends Application
 {
+    private static final String TAG = RibbitApplication.class.getSimpleName();
+
     @Override
     public void onCreate()
     {
@@ -24,14 +31,48 @@ public class RibbitApplication extends Application
 
         Parse.initialize(this, "Dh6Vi5zmPLvWQJgaCGzaIG2PbthfiekHsvuLAYJg", "PuCCi8Ojt01mvj3JwzYz60AzwEu538kIculjIvfj");
 
-        PushService.setDefaultPushCallback(this, MainActivity.class, R.mipmap.ic_stat_ic_launcher);
-        ParseInstallation.getCurrentInstallation().saveInBackground();
+        ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+
+        installation.saveInBackground(new SaveCallback()
+        {
+            @Override
+            public void done(ParseException e)
+            {
+                if(e == null)
+                {
+                    PushService.setDefaultPushCallback(RibbitApplication.this, MainActivity.class, R.mipmap.ic_stat_ic_launcher);
+                    Log.e(TAG, "Parse registration success (launch)");
+                }
+                else
+                {
+                    Log.e(TAG, "Parse registration failed (launch): " + e.getMessage());
+                }
+            }
+        });
     }
 
     public static void updateParseInstallation(ParseUser user)
     {
+        Log.i(TAG, "Attempting to register android installation");
         ParseInstallation installation = ParseInstallation.getCurrentInstallation();
         installation.put(ParseConstants.KEY_USER_ID, user.getObjectId());
-        installation.saveInBackground();
+
+        installation.put("UniqueId", Settings.Secure.ANDROID_ID);
+
+        installation.saveInBackground(new SaveCallback()
+        {
+            @Override
+            public void done(ParseException e)
+            {
+                if(e != null)
+                {
+                    Log.e(TAG, "Parse registration failed (update): " + e.getMessage());
+                }
+                else
+                {
+                    Log.i(TAG, "Parse registration success");
+                }
+            }
+        });
     }
 }
